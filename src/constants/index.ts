f5fffdb136229fc34b04b3a75d88d02a711f4a1c
hirtonsantos <hirtonsantos.hs@gmail.com>
2025-05-09 14:10:14 -0300

    feat: add new constants to view in home

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -113,9 +113,30 @@ export const musicsHistoric = [
     }
 ]
 
+export const frontCourses = [
+    {
+        img: 'https://i.ytimg.com/vi/NprFr1SZGg8/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBJpoQvwA8d8GuWdm4bH8p_2wmvJg',
+        title: 'Responsive Website Design from Scratch',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PL5e68lK9hEzci2jh7qgXDICUARtZ16Ec4'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/8bhp89AIsnc/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDBmy5RP5f3-bcAMrQtyqjJAf5FKg',
+        title: '👨🏻‍💻 Real Front End Interview Questions',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PLX-oxCYw3l8rA5i1BbN_IFOBoBoHG48Cb'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/Luw90ESdSwI/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAQVWKA30LyBkKhMEOHH9q1LKTHmw',
+        title: 'React Design Patterns',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PLgeETUaEEds5HE2uGWezJxYeZwpBonHr-'
+    },
+]
+
 export const jsCourses = [
     {
-        img: 'https://i.ytimg.com/pl_c/PLpwngcHZlPadhRwryAXw3mJWX5KH3T5L3/studio_square_thumbnail.jpg?sqp=CMCd9cAG-oaymwEICKoDEPABSFqi85f_AwYI9_aGuwY=&rs=AOn4CLDO57uGa3rjfugxBj5Gb-HEaM2iNQ',
+        img: 'https://i.ytimg.com/pl_c/PLpwngcHZlPadhRwryAXw3mJWX5KH3T5L3/studio_square_thumbnail.jpg?sqp=CMS3-MAG-oaymwEICNYGEOADSFqi85f_AwYI9_aGuwY=&rs=AOn4CLDdIBGbW3RKRXtrHFViZso9Tjs6tA',
         title: 'JavaScript Projects',
         artists: '',
         link: 'https://www.youtube.com/playlist?list=PLpwngcHZlPadhRwryAXw3mJWX5KH3T5L3'
@@ -125,6 +146,18 @@ export const jsCourses = [
         title: 'Responsive Website Design from Scratch',
         artists: '',
         link: 'https://www.youtube.com/playlist?list=PL5e68lK9hEzci2jh7qgXDICUARtZ16Ec4'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/u7K1sdnCv5Y/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAafbvKiq_bQ44zD4-_Bh7GnTXwRg',
+        title: 'Curso: TypeScript - Zero to Hero',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PLb2HQ45KP0Wsk-p_0c6ImqBAEFEY-LU9H'
+    },
+        {
+        img: 'https://i.ytimg.com/vi/oQPYxhqoH1o/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBgPJweLYD4EuSlVKrnwvHYtDFE7g',
+        title: 'TypeScript',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PLbIBj8vQhvm0Q_TMvL22kkpdUW8sP8Yql'
     }
 ]
 
@@ -173,5 +206,65 @@ export const pythonCourses = [
         title: 'Python',
         artists: 'Pythons',
         link: 'https://www.youtube.com/playlist?list=PLFI0bs5yTX_pTi4Q7svGQRRRWDEpjHKd9'
+    }
+]
+
+export const archSolutionsCourses = [
+    {
+        img: 'https://i.ytimg.com/vi/K4YDHFalAK8/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC4SXAfuX5cvJDvb8IHO-vX0l09yA',
+        title: 'Powershell',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PL7T06JEc5PF6kusr3cg6eYqCAiU6ezVj5'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/njTh_OwMljA/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCZNefEDN9vbFhd2gF8YiJ1_ZXFwg',
+        title: 'Algorithms and Data Structures',
+        artists: 'Pythons',
+        link: 'https://www.youtube.com/playlist?list=PLLXdhg_r2hKA7DPDsunoDZ-Z769jWn4R8'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/mCtCWUWxQaw/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLChv3a01qZy1CWs_Lklr-zoEeVdJA',
+        title: 'Algoritmos',
+        artists: 'Pythons',
+        link: 'https://www.youtube.com/playlist?list=PLBNBxpMAbyhUxkbpblbNjXsPC2PTyp4G0'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/BWprw8UHIzA/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDRN6nGMuF3fTGpSIqRSnnFBIpADQ',
+        title: 'Design Patterns',
+        artists: 'Pythons',
+        link: 'https://www.youtube.com/playlist?list=PLZlA0Gpn_vH_CthENcPCM0Dww6a5XYC7f'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/ie8zUohDoVY/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCzgiGalZi-HWb0mmOAXKXw5oHjYw',
+        title: 'Clean Architecture',
+        artists: 'Pythons',
+        link: 'https://www.youtube.com/playlist?list=PLpJIjBkNnEt_IyTngYRkXXwxqz-Ae74ub'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/EumXak7TyQ0/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBdFjdU8MfEtWXK1em0TLORPuTp1Q',
+        title: 'Clean Code',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PLZlA0Gpn_vH-xGQ-nQ87rXI7QkM6W3E79'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/FQ61M65Kwwk/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAd_2fWGhIvJv7YGHUfn8kFB3Xx8g',
+        title: "System Design Beginner's Course 2022",
+        artists: 'Pythons',
+        link: 'https://www.youtube.com/playlist?list=PL8hP5HjAnJ3_mT7IHXjlbpYX_xiz4v_kP'
+    },
+    {
+        img: 'https://i.ytimg.com/vi/PWGjcXebEjY/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBhMh68II6ba7R8I8IgeXQS1XxSyw',
+        title: 'System Design Tutorials',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PL8hP5HjAnJ3-tAOvAl7Al4pAwLgvbssHC'
+    }
+]
+
+export const englishCourses = [
+    {
+        img: 'https://i.ytimg.com/vi/rEcvaLStb1c/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDuCAV7zKvR5OovLoVEVZaQZbOqPQ',
+        title: 'APRENDER INGLÊS COM TEXTOS E DIÁLOGOS',
+        artists: '',
+        link: 'https://www.youtube.com/playlist?list=PLVRdJEqvAMbccRcixguBFkQuuW0mC_nKn'
     }
 ]
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,13 +3,23 @@ import { useState } from "react";
 import { Header } from "../../components/header";
 import UserPreferences from "../../components/userPreferences";
 import Category from "../../components/category";
-import { catolicism, javaCourses, jsCourses, pythonCourses } from "../../constants";
+import {
+  archSolutionsCourses,
+  catolicism,
+  englishCourses,
+  frontCourses,
+  javaCourses,
+  jsCourses,
+  pythonCourses,
+} from "../../constants";
 import { ChevronLeft, ChevronRight } from "lucide-react"; // ou use ícones próprios
 
 const categories = [
-  { list: pythonCourses, label: "Python Courses" },
-  { list: javaCourses, label: "Java Courses" },
-  { list: jsCourses, label: "Javascript Courses" },
+  { list: pythonCourses, label: "Python Training" },
+  { list: javaCourses, label: "Java Training" },
+  { list: archSolutionsCourses, label: 'Arch Solutions' },
+  { list: jsCourses, label: "Javascript Training" },
+  { list: frontCourses, label: 'Frontend Training'},
 ];
 
 const Home = () => {
@@ -20,32 +30,52 @@ const Home = () => {
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prev) => (prev - 1 + categories.length) % categories.length);
+    setCurrentIndex(
+      (prev) => (prev - 1 + categories.length) % categories.length
+    );
   };
 
   return (
     <div className="container-home" data-testid="home-route">
-        <Header />
-        <UserPreferences data-testid="user-profile" />
+      <Header />
+      <UserPreferences data-testid="user-profile" />
 
-        <div className="category-carousel" style={{alignItems: 'flex-start', width: '80%'}}>
-            <Category categoryList={catolicism} text="Catolicismo" style={{height: 'auto'}} />
-        </div>
+      <div
+        className="category-carousel"
+        style={{ alignItems: "flex-start", width: "80%" }}
+      >
+        <Category
+          categoryList={catolicism}
+          text="Catolicismo"
+          style={{ height: "auto" }}
+        />
+      </div>
 
-        <div className="category-carousel">
-            <button onClick={handlePrev} className="arrow-button">
-                <ChevronLeft />
-            </button>
+      <div
+        className="category-carousel"
+        style={{ alignItems: "flex-start", width: "80%" }}
+      >
+        <Category
+          categoryList={englishCourses}
+          text="English Courses"
+          style={{ height: "auto" }}
+        />
+      </div>
 
-            <Category
-                categoryList={categories[currentIndex].list}
-                text={categories[currentIndex].label}
-            />
+      <div className="category-carousel">
+        <button onClick={handlePrev} className="arrow-button">
+          <ChevronLeft />
+        </button>
 
-            <button onClick={handleNext} className="arrow-button">
-                <ChevronRight />
-            </button>
-        </div>
+        <Category
+          categoryList={categories[currentIndex].list}
+          text={categories[currentIndex].label}
+        />
+
+        <button onClick={handleNext} className="arrow-button">
+          <ChevronRight />
+        </button>
+      </div>
     </div>
   );
 };
